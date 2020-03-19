5604e4f5e148730d5b0e8c498742306cd99b39b6
Vikiking <zanzirop@mail.ru>
2020-03-20 00:21:15 +0300

    Add project HelloWorld

---
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn = (Button) findViewById(R.id.btn);
+        btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(this, "Hello world!", Toast.LENGTH_SHORT).show();
+    }
+}
